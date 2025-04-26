32e04aa80758726af1280e9c590dbaa789cd671f
clearlyclear-mbremer <marty@clearlyclear.com>
2025-04-25 19:40:40 -0500

    Update embed_docs.py

--- embed_docs.py
+++ embed_docs.py
@@ -4,7 +4,6 @@ import requests
 import time
 
 from langchain_community.document_loaders import ConfluenceLoader
-from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
